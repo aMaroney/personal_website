56a04fde729ac896d81e24353076829695022641
aMaroney <Amaroney92@gmail.com>
2018-02-18 15:36:40 -0500

    Personal Webiste v2

--- personal_website/personal/views.py
+++ personal_website/personal/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.base import *
 
 def index(request):
     return render(request, 'personal/home.html')
@@ -6,3 +8,7 @@ def index(request):
 def contact(request):
     return render(request, 'personal/basic.html', {'content':['If you would like to contact me, my email address is AMaroney92@gmail.com'
                                                               '','']})
+
+def weather(request):
+    pull_weather_file = HttpResponse(open('weather_for_website.txt').read(), content_type='text/plain; utf-8')
+    return pull_weather_file
